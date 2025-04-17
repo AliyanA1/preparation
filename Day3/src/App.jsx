f1b7e41cbb2e7e4f51b0f999e9aa5b14087550a0
Aliyan Ali <darkpikayt1@gmail.com>
2025-04-17 08:52:39 +0500

    App jsx

---
+++ Day3/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InputForm from './Components/InputForm'
+
+const App = () => {
+  return (
+    <div>
+      <InputForm/>
+    </div>
+  )
+}
+
+export default App

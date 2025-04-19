39e23937488ecacb821a3099712f6521285930d5
Aliyan Ali <darkpikayt1@gmail.com>
2025-04-19 06:43:27 +0500

    Data js

---
+++ Day4/src/Data/Data.js
@@ -0,0 +1,9 @@
+import { images } from "../assets/imort"
+
+export const data=[
+    {id:1,title:'pc', discription:'img of a pc', imgUrl:images.img3},
+    {id:1, title:'porsche',discription:'img of a porsche', imgUrl:images.img2},
+    {id:1, title:'Anime',discription:'img of a Anime', imgUrl:images.img1},
+    {id:1, title:'Anime',discription:'img of a Aot', imgUrl:images.img4},
+    {id:1, title:'Anime',discription:'img of a Swordsman', imgUrl:images.img5}
+]

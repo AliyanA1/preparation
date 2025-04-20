e2edfa991e10bdfe3b059e395d7cb039aa81197a
AliyanA1 <124417710+AliyanA1@users.noreply.github.com>
2025-04-20 20:38:02 +0500

    Update App.jsx

--- Day2/src/App.jsx
+++ Day2/src/App.jsx
@@ -2,7 +2,7 @@ import Card from './Components/Card'
 import {images} from './assets/imort'
 const App=()=>{
   return(
-    <div className='h-full bg-amber-200 p-2'>
+    <main className='h-full bg-amber-200 p-2'>
       
       <div className=' grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 '>
       <Card  title='picture' discription='the picture about anime' imgUrl={images.img1} />
@@ -12,7 +12,7 @@ const App=()=>{
        <Card  title='Anime' discription='katana Swords Man' imgUrl={images.img5}/>
       </div>
 
-    </div>
+    </main>
   )
 }
 

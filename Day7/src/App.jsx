c7a576fbd017dd3c2cc7d22fc1da82bc20022670
Aliyan Ali <darkpikayt1@gmail.com>
2025-04-26 12:36:16 +0500

    App jsx

--- Day7/src/App.jsx
+++ Day7/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ShowDetails from './Components/Task1'
+import Task2 from './Components/Task2'
 
 const App = () => {
   return (
     <div className='h-screen flex justify-center items-center bg-gray-900'>
-      <h1 className='text-2xl text-white font-mono'>Hey its day7 of learning React and  revising</h1>
+      {/* <ShowDetails/> */}
+      <Task2/>
     </div>
   )
 }

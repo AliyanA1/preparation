7782583600e4f5db794552c68d9a1a91efdad408
Aliyan Ali <darkpikayt1@gmail.com>
2025-04-26 11:08:34 +0500

    App jsx

---
+++ Day7/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen flex justify-center items-center bg-gray-900'>
+      <h1 className='text-2xl text-white font-mono'>Hey its day7 of learning React and  revising</h1>
+    </div>
+  )
+}
+
+export default App
